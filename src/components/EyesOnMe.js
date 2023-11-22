c3f14e1fa0c4de9d93f878646d61f329298ec054
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-11-22 11:10:31 -0800

    lab complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+function HandleBtn () {
+    return <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+}
+
+function handleFocus () {
+    console.log('Good!')
+}
+
+function handleBlur () {
+    console.log('Hey! Eyes on me!')
+}
+
+
+
+function EyesOnMe () {
+    return (
+        <HandleBtn />
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
+function HandleChange() {
+    return <input type = {'password'} onChange={handleChange}></input>
+}
+
+function handleChange() {
+    console.log('Entering password...')
+}
+
 function Keypad (){
     return (
-        <div></div>
+        <div>
+           <HandleChange />
+        </div>
     )
 }
 
